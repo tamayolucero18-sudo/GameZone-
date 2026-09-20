09c9b7543f2373e0eda0b9730eb1685d7be2923f
tamayolucero18-sudo <tamayolucero18@gmail.com>
2026-09-20 11:23:32 -0600

    sm

--- index.php
+++ index.php
@@ -74,7 +74,6 @@ $tieneModal = $juegoAbierto || $carritoAbierto || $checkoutAbierto || $configAbi
         <?php endif; ?>
       </a>
       <a class="header__action-btn header__action-btn--text header__action-btn--login" href="<?= url(['login' => 1], 'index.php') ?>">
-        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" width="22" height="22"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
         <span>Acceder</span>
       </a>
     </div>
